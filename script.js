9b55b7b32033e6d48502294a6db07a9acebe3519
Gurpreet Singh <Harryghotra195@gmail.com>
2025-02-05 23:43:23 -0500

    added new css and index files, also added projects & education section

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".tab-button");
+  const buttons = document.querySelectorAll(".tab-button");
+  const workTab = document.getElementById("workTab");
+  const eduTab = document.getElementById("eduTab");
+  const projectsSection = document.querySelector(".wok-exp");
+  const educationSection = document.getElementById("education-section");
 
-    buttons.forEach(button => {
-        button.addEventListener("click", function () {
-            buttons.forEach(btn => btn.classList.remove("active"));
-            this.classList.add("active");
-        });
+  educationSection.classList.add("hidden");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      buttons.forEach((btn) => btn.classList.remove("active"));
+
+      this.classList.add("active");
+
+      if (this.id === "workTab") {
+        projectsSection.classList.remove("hidden");
+        educationSection.classList.add("hidden");
+      } else {
+        educationSection.classList.remove("hidden");
+        projectsSection.classList.add("hidden");
+      }
     });
+  });
 });
